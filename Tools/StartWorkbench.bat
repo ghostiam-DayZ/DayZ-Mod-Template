e9cbf12c6b2b3fd1926f22a6ebd5256a108aa8b5
GhostIAm <ghostrussian@gmail.com>
2020-07-28 01:35:32 +0900

    fix little bug

--- Tools/StartWorkbench.bat
+++ Tools/StartWorkbench.bat
@@ -41,8 +41,9 @@ set mods=
 if "%AdditionalWorkbenchMods%"=="" (
     set mods=%wd%\%Prefix%\%ModName%
 ) else (
-    set mods=%AdditionalWorkbenchMods%;%modName%
+    set mods=%AdditionalWorkbenchMods%;%wd%\%Prefix%\%ModName%
 )
+
 for %%a in ("%mods:;=" "%") do (
     set mod=%%~a
 	set modDir=%wd%\%Prefix%\%ModName%\
